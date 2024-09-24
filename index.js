ccff10bc5709bed3ce38dd26adea0d5a1172519a
shamim23m <shamimmohamud13@gmail.com>
2024-09-24 11:08:49 +0300

    assingment

--- index.js
+++ index.js
@@ -1,11 +1,40 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(ride){
   // Write your code here!
+  
+  if (ride > 2500) {
+    return 'No can do.';
+  }
+  else if (ride > 2000) {
+    return 'I will gladly take your thirty bucks.';
+  }
+  else if (ride > 400) {
+    return 'That will be twenty bucks.';
+  }
+  else {
+    return 'This one is on me!';
+  }
+
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+  if (city === 'NYC') {
+    return 'Ok, sounds good.';
+  }
+  else {
+    return 'No go.';
+  }
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  if (tip === 'generous') {
+    return 'Thank you so much.';
+  }
+  else if (tip === 'not as generous') {
+    return 'Thank you.';
+  }
+  else {
+    return 'Bye.';
+  }
 }
